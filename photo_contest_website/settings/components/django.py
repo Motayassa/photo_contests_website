82106fefc499dac4975dcfaaecdb4fd1eb2ec3a3
Motayassa <malovilyajob@gmail.com>
2024-03-05 22:57:50 +0300

    Activate the api application

--- photo_contest_website/settings/components/django.py
+++ photo_contest_website/settings/components/django.py
@@ -17,6 +17,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "api",
 ]
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
