8175f9b231876195dc694cb6d6a5cf4911f0c870
Motayassa <malovilyajob@gmail.com>
2024-03-11 16:44:01 +0300

    Add a one-to-many relationship for photo and likes

--- models_app/models/like/models.py
+++ models_app/models/like/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from models import photo
 
 
 class Like(models.Model):
-    pass
     #  user_id
-    #  photo_id
+    photo = models.ForeignKey(
+        photo, on_delete=models.CASCADE, related_name="photo_like"
+    )
