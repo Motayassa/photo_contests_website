07370f6926c26020db243428b0f184b9ca2861b7
Motayassa <malovilyajob@gmail.com>
2024-03-11 17:17:38 +0300

    Add one to many relationship for like and like author

--- models_app/models/like/models.py
+++ models_app/models/like/models.py
@@ -1,9 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
 from models import photo
 
 
 class Like(models.Model):
-    #  user_id
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="likes_author"
+    )
     photo = models.ForeignKey(
         photo, on_delete=models.CASCADE, related_name="photo_like"
     )
