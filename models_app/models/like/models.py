d9e5c4819250c12b468707134e135bae4f690589
Motayassa <malovilyajob@gmail.com>
2024-03-14 21:01:04 +0300

    Add dates for likes model.

--- models_app/models/like/models.py
+++ models_app/models/like/models.py
@@ -11,6 +11,9 @@ class Like(models.Model):
     photo = models.ForeignKey(
         Photo, on_delete=models.CASCADE, related_name="photos_like"
     )
+    created_date = models.DateTimeField(auto_now_add=True, null=True)
+    updated_date = models.DateTimeField(auto_now=True, null=True)
 
     class Meta:
         app_label = "api"
+        ordering = ["created_date", "updated_date"]
