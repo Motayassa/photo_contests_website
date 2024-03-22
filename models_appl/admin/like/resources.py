85f60aee588714fbf8280bc13bfb16d437f4a1b3
Motayassa <malovilyajob@gmail.com>
2024-03-22 16:09:02 +0300

    Fix searches_fields work for admin panel.

--- models_appl/admin/like/resources.py
+++ models_appl/admin/like/resources.py
@@ -7,5 +7,5 @@ from models_appl.models.like.models import Like
 class LikeAdmin(admin.ModelAdmin):
     list_display = ["author", "photo", "created_date", "updated_date"]
     list_filter = ["created_date", "updated_date", "author", "photo"]
-    search_fields = ["created_date", "updated_date", "author", "photo"]
+    search_fields = ["created_date", "updated_date", "author__username", "photo__title"]
     ordering = ["created_date", "updated_date", "author", "photo"]
