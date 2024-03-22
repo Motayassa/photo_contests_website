150cc076db6ac9feb384bf53f4070867085e6277
Motayassa <malovilyajob@gmail.com>
2024-03-22 14:07:43 +0300

    Implement the creation of an image thumbnail

--- models_appl/models/photo/models.py
+++ models_appl/models/photo/models.py
@@ -1,6 +1,10 @@
+from io import BytesIO
+
 from django.contrib.auth.models import User
+from django.core.files import File
 from django.db import models
 from django.utils import timezone
+from PIL import Image
 
 
 class Photo(models.Model):
@@ -15,13 +19,12 @@ class Photo(models.Model):
     )
     title = models.CharField(max_length=50)
     photos_image = models.ImageField(upload_to="images/%Y/%m/%d/")
-    photos_image200px = models.ImageField(upload_to="images/%Y/%m/%d/")
-    #  photos_image200px необходимо подцмать над размером и тем, как редачить фото
-    # нужен ли оригинальный url фотографии?
-    url = models.URLField()
+    thumbnail300px = models.ImageField(
+        upload_to="images_thumbnail/%Y/%m/%d/", null=True
+    )
     description = models.TextField(max_length=1000)
-    likes_amount = models.IntegerField()
-    comments_amount = models.IntegerField()
+    likes_amount = models.IntegerField(default=0)
+    comments_amount = models.IntegerField(default=0)
     publicate_date = models.DateTimeField(default=timezone.now)
     add_date = models.DateTimeField(auto_now_add=True)
     status = models.CharField(
@@ -32,6 +35,29 @@ class Photo(models.Model):
         indexes = [models.Index(fields=["-add_date"])]
         ordering = ["-add_date"]
         app_label = "api"
+
+    def save(self, *args, **kwargs):
+        self.thumbnail300px = self.make_thumbnail(self.photos_image)
+        super().save(*args, **kwargs)
+
+    def valid_extension(self, _img):
+        if ".jpg" in _img:
+            return "JPEG"
+        elif ".jpeg" in _img:
+            return "JPEG"
+        elif ".png" in _img:
+            return "PNG"
+
+    def make_thumbnail(self, image):
+        fixed_width = 300
+        im = Image.open(image)
+        width_percent = fixed_width / float(im.size[0])
+        height_size = int(float(im.size[0]) * float(width_percent))
+        im = im.resize((fixed_width, height_size), Image.ANTIALIAS)
+        im_io = BytesIO()
+        im.save(im_io, self.valid_extension(image.name), optimize=True, quality=99)
+        thumbnail = File(im_io, name=image.name)
+        return thumbnail
 
     def __str__(self):
         return self.title
